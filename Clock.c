4d525db0e281d8ef49dea612e0fcc711d71fc4ad
EnzoAstori <154561593+EnzoAstori@users.noreply.github.com>
2023-12-26 02:12:56 -0300

    Update Clock.c

--- Clock.c
+++ Clock.c
@@ -3,14 +3,14 @@
 #include <unistd.h>
 
 int main(void){
-    while (1){
+    while(1){
 
         time_t Relogio;
         struct tm *info;
         time(&Relogio);
         info=localtime(&Relogio);
 
-        printf("Hora local: %02d:%02d:%02d\r",info->tm_hour, info->tm_min, info->tm_sec);
+        printf("Hora local:%02d:%02d:%02d\r",info->tm_hour, info->tm_min, info->tm_sec);
 
         fflush(stdout); 
 
